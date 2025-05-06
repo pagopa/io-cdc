a340837e47ce619c2463035337536f7e4247b8f4
michaeldisaro <michael.disaro@gmail.com>
2025-05-06 16:09:14 +0200

    removed runner folder

--- infra/github-runner/prod/data.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_container_app_environment" "cae" {
-  name                = "${local.prefix}-${local.env_short}-itn-github-runner-cae-01"
-  resource_group_name = "${local.prefix}-${local.env_short}-itn-github-runner-rg-01"
-}
