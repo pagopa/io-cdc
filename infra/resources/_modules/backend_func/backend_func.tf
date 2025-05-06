dc5974800b570cb5e36228d7026c1a9431ea4784
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2025-05-06 14:06:53 +0200

    [#IOBP-1590] Boilerplate for backend functions and fims sdk (#1)

--- infra/resources/_modules/backend_func/backend_func.tf
+++ infra/resources/_modules/backend_func/backend_func.tf
@@ -12,7 +12,7 @@ module "cdc_backend_func" {
   }
 
   resource_group_name = var.resource_group_name
-  health_check_path   = "/api/v1/cdc/info"
+  health_check_path   = "/info"
   node_version        = 20
 
   subnet_id                            = var.subnet_id
