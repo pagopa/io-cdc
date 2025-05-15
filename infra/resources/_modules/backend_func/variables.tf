9e83f1cbdddd837d01ba57880b3408d919eda61e
Lorenzo Curreli <104564805+loricurreli@users.noreply.github.com>
2025-05-15 14:30:07 +0200

    Removed io-cdc-fe folder (#5)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    Moved frontend app into frontend folder and remove io-cdc-fe folder
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ---------
    
    Co-authored-by: Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
    Co-authored-by: michaeldisaro <michael.disaro@gmail.com>
    Co-authored-by: PierpaoloPirasSp <pierpaolo.piras@spindox.it>

---
+++ infra/resources/_modules/backend_func/variables.tf
@@ -0,0 +1,126 @@
+variable "prefix" {
+  type        = string
+  description = "IO Prefix"
+}
+
+variable "env_short" {
+  type        = string
+  description = "Short environment"
+}
+
+variable "project" {
+  type        = string
+  description = "IO prefix and short environment"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "domain" {
+  type        = string
+  description = "Domain"
+}
+
+variable "instance_number" {
+  type        = string
+  description = "The istance number to create"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Resource tags"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group where resources will be created"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "A predefined subnet id"
+  default     = null
+}
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "Private Endpoints subnet Id"
+}
+
+variable "virtual_network" {
+  type = object({
+    name                = string
+    resource_group_name = string
+  })
+  description = "Virtual network to create subnet in"
+}
+
+variable "private_dns_zone_resource_group_name" {
+  type        = string
+  description = "Resource group name of the private DNS zone to use for private endpoints"
+}
+
+variable "ai_instrumentation_key" {
+  type        = string
+  description = "The key to connect to application insights"
+}
+
+variable "ai_connection_string" {
+  sensitive   = true
+  type        = string
+  description = "The connection string to connect to application insights"
+}
+
+variable "ai_sampling_percentage" {
+  type        = string
+  description = "The sampling percentage for application insights"
+}
+
+# REPO DEFINED VARIABLES
+variable "nat_gateway_id" {
+  type        = string
+  description = "The ID of the NAT Gateway"
+}
+
+variable "cosmosdb_cdc_uri" {
+  type        = string
+  description = "Connection uri for CdC cosmosdb"
+}
+
+variable "cosmosdb_cdc_key" {
+  type        = string
+  description = "Connection key for CdC cosmosdb"
+}
+
+variable "cosmosdb_cdc_database_name" {
+  type        = string
+  description = "Database name for CdC cosmosdb"
+}
+
+variable "services_api_url" {
+  type        = string
+  description = "Services API url"
+}
+
+variable "services_api_key" {
+  type        = string
+  description = "Services API key"
+}
+
+variable "redis_url" {
+  type        = string
+  description = "Redis url"
+}
+
+variable "redis_port" {
+  type        = string
+  description = "Redis port"
+}
+
+variable "redis_password" {
+  type        = string
+  description = "Redis password"
+}
+
