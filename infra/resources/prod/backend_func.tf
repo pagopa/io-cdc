80230896f8d9390ef72af87c1d19ebfe39c05616
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2025-05-22 16:12:05 +0200

    [#IOBP-1668 A] Added secrets (#9)
    #### List of Changes
    Added secrets and infra to make a JWT.
    
    #### Motivation and Context
    We need to sign JWT to send to external parties.

--- infra/resources/prod/backend_func.tf
+++ infra/resources/prod/backend_func.tf
@@ -22,15 +22,22 @@ module "backend_func" {
     name                = data.azurerm_virtual_network.vnet_common_itn.name
   }
 
-  cosmosdb_cdc_uri = module.cosmos_db.cosmos_db.endpoint
-  cosmosdb_cdc_key = ""
-  cosmosdb_cdc_database_name = ""
+  cosmosdb_cdc_uri           = module.cosmos_db.cosmos_db.endpoint
+  cosmosdb_cdc_key           = data.azurerm_key_vault_secret.cosmosdb_cdc_key.value
+  cosmosdb_cdc_database_name = data.azurerm_key_vault_secret.cosmosdb_cdc_database_name.value
 
-  services_api_url = ""
-  services_api_key = ""
+  services_api_url = data.azurerm_key_vault_secret.services_api_url.value
+  services_api_key = data.azurerm_key_vault_secret.services_api_key.value
+
+  fims_base_url      = data.azurerm_key_vault_secret.fims_base_url.value
+  fims_client_id     = data.azurerm_key_vault_secret.fims_client_id.value
+  fims_client_secret = data.azurerm_key_vault_secret.fims_client_secret.value
+
+  jwt_public_key  = data.azurerm_key_vault_secret.jwt_public_key.value
+  jwt_private_key = data.azurerm_key_vault_secret.jwt_private_key.value
 
   nat_gateway_id = data.azurerm_nat_gateway.itn_ng.id
-  
+
   redis_url      = module.redis_cdc.hostname
   redis_port     = module.redis_cdc.ssl_port
   redis_password = module.redis_cdc.primary_access_key
