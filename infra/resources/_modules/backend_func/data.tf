e9743110f5ad10eb8919bb758b0fe58e68904b7d
michaeldisaro <michael.disaro@gmail.com>
2025-05-29 13:39:27 +0200

    fixed review

--- infra/resources/_modules/backend_func/data.tf
+++ infra/resources/_modules/backend_func/data.tf
@@ -2,7 +2,3 @@ data "azurerm_api_management" "apim_platform" {
   name                = "io-p-itn-platform-api-gateway-apim-01"
   resource_group_name = "io-p-itn-common-rg-01"
 }
-
-data "http" "cdc_backend_func_openapi" {
-  url = "https://raw.githubusercontent.com/pagopa/io-cdc/refs/heads/main/apps/backend-func/api/internal.yaml"
-}
