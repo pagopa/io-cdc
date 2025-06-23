55f211e07ed1cf0e8232c59db2e8d5ba5b1b4908
michaeldisaro <michael.disaro@gmail.com>
2025-06-23 16:17:10 +0200

    fixed var

--- apps/backend-func/src/utils/queue.ts
+++ apps/backend-func/src/utils/queue.ts
@@ -43,6 +43,6 @@ export class QueueStorage {
 
   constructor(config: Config) {
     this.config = config;
-    this.queueService = new QueueService(this.config.STORAGE_CONNECTION_STRING);
+    this.queueService = new QueueService(this.config.STORAGE_ACCOUNT_CONNECTION_STRING);
   }
 }
