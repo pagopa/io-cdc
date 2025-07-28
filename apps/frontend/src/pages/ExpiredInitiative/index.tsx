ac4bb35db6d9f6fdab2434bd74614686eb259b44
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2025-07-28 10:38:08 +0200

    feat: add path to close in app browser

--- apps/frontend/src/pages/ExpiredInitiative/index.tsx
+++ apps/frontend/src/pages/ExpiredInitiative/index.tsx
@@ -1,16 +1,17 @@
 import { Icon } from '@io-cdc/ui';
 import { Button, Stack, Typography } from '@mui/material';
 import { EXPIRED_INITIATIVE_CONFIG_MAP } from './constants';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const STATUSES = {
   500: 'expired',
   501: 'alreadyRequested',
 };
 
-//TODO -> define what should be done on close
 const ExpiredInitiative = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
+
   const status = STATUSES[(state.status as keyof typeof STATUSES) ?? 500];
   const { image, description, title } = EXPIRED_INITIATIVE_CONFIG_MAP[status];
   return (
@@ -22,7 +23,7 @@ const ExpiredInitiative = () => {
         </Typography>
         {description && <Typography textAlign="center">{description}</Typography>}
       </Stack>
-      <Button onClick={() => window.location.reload()} size="small" variant="contained">
+      <Button onClick={() => navigate('iossoapi://cancel')} size="small" variant="contained">
         Chiudi
       </Button>
     </Stack>
--- apps/frontend/src/pages/Feedback/index.tsx
+++ apps/frontend/src/pages/Feedback/index.tsx
@@ -1,11 +1,11 @@
 import { Icon, OperationResult } from '@io-cdc/ui';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Box, Button, Stack } from '@mui/material';
 import { CONFIG_BY_STATUS, FeedbackState } from './constants';
 
-//TODO -> define what should be done on close
 const Feedback = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
   const { title, description, icon, subTitle } = CONFIG_BY_STATUS[(state as FeedbackState).status];
 
   return (
@@ -13,7 +13,7 @@ const Feedback = () => {
       {icon && <Icon name={icon} sx={{ width: 60, height: 60 }} />}
       <OperationResult title={title} subTitle={subTitle} description={description} />
       <Box>
-        <Button onClick={() => window.location.reload()} variant="contained">
+        <Button onClick={() => navigate('iossoapi://cancel')} variant="contained">
           Chiudi
         </Button>
       </Box>
