afcf76949bcf5d397fff002f35f1ecf0a1711b7f
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2025-07-30 17:13:30 +0200

    fix: read device instead of deviceId

--- apps/frontend/src/hooks/useMixPanelSession.ts
+++ apps/frontend/src/hooks/useMixPanelSession.ts
@@ -5,7 +5,7 @@ import { initAnalytics } from '@io-cdc/mixpanel';
 export const useMixPanelSession = () => {
   const { search } = useLocation();
 
-  const deviceId = useMemo(() => new URLSearchParams(search).get('deviceId'), [search]);
+  const deviceId = useMemo(() => new URLSearchParams(search).get('device'), [search]);
 
   useEffect(() => {
     if (deviceId) {
