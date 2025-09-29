f303b8b52aa4a49a47ef60f5cd1fe24940fa2af9
michaeldisaro <michael.disaro@gmail.com>
2025-09-29 14:17:37 +0200

    scaling for prod

--- infra/resources/_modules/cosmos_db/cosmos_db.tf
+++ infra/resources/_modules/cosmos_db/cosmos_db.tf
@@ -45,7 +45,7 @@ resource "azurerm_cosmosdb_sql_container" "card_requests" {
   database_name       = azurerm_cosmosdb_sql_database.cdc_cosmos_db.name
   partition_key_paths = ["/fiscalCode"]
   autoscale_settings {
-    max_throughput = 10000
+    max_throughput = 12000
   }
 }
 
@@ -57,6 +57,6 @@ resource "azurerm_cosmosdb_sql_container" "requests-audit" {
   database_name       = azurerm_cosmosdb_sql_database.cdc_cosmos_db.name
   partition_key_paths = ["/fiscalCode"]
   autoscale_settings {
-    max_throughput = 10000
+    max_throughput = 12000
   }
 }
