48a7219315a0233b90159ba4e00d9ed9e3a590b3
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2025-10-13 14:23:46 +0200

    [#IOBP-2051] Added get voucher function (#88)

--- apps/backend-func/src/__mocks__/cdc.mock.ts
+++ apps/backend-func/src/__mocks__/cdc.mock.ts
@@ -13,9 +13,15 @@ export const getCdcVouchersTEMock = vi.fn().mockReturnValue(TE.of([]));
 
 export const postCdcVouchersMock = vi.fn().mockReturnValue(TE.of({}));
 
+export const getCdcVoucherTEMock = vi.fn().mockReturnValue(TE.of({}));
+
+export const deleteCdcVoucherTEMock = vi.fn().mockReturnValue(TE.of(true));
+
 export const CdcUtilsMock = {
+  deleteCdcVoucherTE: deleteCdcVoucherTEMock,
   getAlreadyRequestedYearsCdcTE: getAlreadyRequestedYearsCdcTEMock,
   getCdcCardsTE: getCdcCardsTEMock,
+  getCdcVoucherTE: getCdcVoucherTEMock,
   getCdcVouchersTE: getCdcVouchersTEMock,
   postCdcVouchersTE: postCdcVouchersMock,
   requestCdcTE: requestCdcTEMock,
