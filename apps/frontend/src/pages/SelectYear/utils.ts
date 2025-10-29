d8054a636ac4a70a15ded098cdc5badb764f5df6
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2025-10-29 12:28:21 +0100

    chore: remove comments

--- apps/frontend/src/pages/SelectYear/utils.ts
+++ apps/frontend/src/pages/SelectYear/utils.ts
@@ -2,7 +2,6 @@ import { isAfter } from 'date-fns';
 
 export const checkExpirationDate: () => Boolean = () => {
   // Limit timestamp in UTC
-  // const limitUTC = new Date('2025-10-31T10:59:59.999Z');
   const limitUTC = new Date('2025-10-31T10:59:59.999Z');
 
   // Current time in the target timezone
