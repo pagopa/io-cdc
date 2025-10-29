a126580b3c32ae72b7a8af32a05bb92f64b27024
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2025-10-29 12:05:23 +0100

    fix: fix dates

--- apps/frontend/src/pages/SelectYear/utils.ts
+++ apps/frontend/src/pages/SelectYear/utils.ts
@@ -1,17 +1,13 @@
-export const checkExpirationDate: () => Boolean = () => {
-  // Target timezone
-  const timezone = 'Europe/Rome';
+import { isAfter } from 'date-fns';
 
+export const checkExpirationDate: () => Boolean = () => {
   // Limit timestamp in UTC
   // const limitUTC = new Date('2025-10-31T10:59:59.999Z');
-  const limitUTC = new Date('2025-10-29T10:59:59.999Z');
+  const limitUTC = new Date('2025-10-31T10:59:59.999Z');
 
   // Current time in the target timezone
-  const nowInZone = new Date(new Date().toLocaleString('en-US', { timeZone: timezone }));
-
-  // Convert limit UTC to the target timezone
-  const limitInZone = new Date(limitUTC.toLocaleString('en-US', { timeZone: timezone }));
+  const now = new Date();
 
   // Compare
-  return nowInZone <= limitInZone;
+  return isAfter(limitUTC, now);
 };
