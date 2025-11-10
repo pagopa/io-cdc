fa62e6e1d241d1af89bf43369117193dec975b37
michaeldisaro <michael.disaro@gmail.com>
2025-11-10 12:46:12 +0100

    changed test session to return session id

--- apps/backend-func/src/functions/test-session.ts
+++ apps/backend-func/src/functions/test-session.ts
@@ -1,7 +1,6 @@
 import * as H from "@pagopa/handler-kit";
 import { httpAzureFunction } from "@pagopa/handler-kit-azure-func";
 import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings.js";
-import { enumType } from "@pagopa/ts-commons/lib/types.js";
 import * as crypto from "crypto";
 import * as RTE from "fp-ts/lib/ReaderTaskEither.js";
 import * as TE from "fp-ts/lib/TaskEither.js";
@@ -9,10 +8,6 @@ import { pipe } from "fp-ts/lib/function.js";
 import * as t from "io-ts";
 
 import { Config } from "../config.js";
-import {
-  RouteEnum,
-  SessionToken,
-} from "../generated/definitions/internal/SessionToken.js";
 import { withParams } from "../middlewares/withParams.js";
 import { Session } from "../models/session.js";
 import {
@@ -22,6 +17,7 @@ import {
 } from "../utils/errors.js";
 import { toHash } from "../utils/hash.js";
 import { RedisClientFactory } from "../utils/redis.js";
+import { setWithExpirationTask } from "../utils/redis_storage.js";
 import { storeSessionTe } from "../utils/session.js";
 
 interface Dependencies {
@@ -33,22 +29,36 @@ const Body = t.type({
   family_name: NonEmptyString,
   fiscal_code: FiscalCode,
   given_name: NonEmptyString,
-  route: enumType<RouteEnum>(RouteEnum, "route"),
 });
 type Body = t.TypeOf<typeof Body>;
 
 export const getSessionToken =
-  (testUser: Session, route: RouteEnum) =>
-  (deps: Dependencies): TE.TaskEither<ResponseError, SessionToken> =>
+  (testUser: Session) =>
+  (deps: Dependencies): TE.TaskEither<ResponseError, string> =>
     pipe(
-      TE.of(crypto.randomBytes(32).toString("hex")),
-      TE.chainFirst((sessionToken) =>
-        storeSessionTe(deps.redisClientFactory, sessionToken, testUser),
+      TE.Do,
+      TE.bind("sessionId", () => TE.of(crypto.randomBytes(32).toString("hex"))),
+      TE.bind("sessionToken", () =>
+        TE.of(crypto.randomBytes(32).toString("hex")),
       ),
-      TE.chain((sessionToken) =>
+      TE.chainFirst(({ sessionId, sessionToken }) =>
+        pipe(
+          storeSessionTe(deps.redisClientFactory, sessionToken, testUser),
+          TE.chain(() =>
+            // bind one time session id to session token
+            setWithExpirationTask(
+              deps.redisClientFactory,
+              sessionId,
+              sessionToken,
+              60,
+            ),
+          ),
+        ),
+      ),
+      TE.chain(({ sessionId }) =>
         pipe(toHash(testUser.fiscal_code), (cfHash) =>
           deps.config.TEST_USERS.includes(cfHash)
-            ? TE.of({ route, token: sessionToken })
+            ? TE.of(sessionId)
             : TE.left(new Error("User not allowed")),
         ),
       ),
@@ -58,14 +68,14 @@ export const getSessionToken =
 export const makeTestSessionHandler: H.Handler<
   H.HttpRequest,
   | H.HttpResponse<H.ProblemJson, H.HttpErrorStatusCode>
-  | H.HttpResponse<SessionToken, 200>,
+  | H.HttpResponse<string, 200>,
   Dependencies
 > = H.of((req) =>
   pipe(
     withParams(Body, req.body),
     RTE.mapLeft(errorToValidationError),
-    RTE.chain(({ route, ...testUser }) =>
-      pipe(getSessionToken(testUser, route), RTE.map(H.successJson)),
+    RTE.chain((testUser) =>
+      pipe(getSessionToken(testUser), RTE.map(H.successJson)),
     ),
     responseErrorToHttpError,
   ),
