7a229091b225e0b9aa9d8ef0dc6f79a30454db23
michaeldisaro <michael.disaro@gmail.com>
2025-12-04 16:44:01 +0100

    added caller to attributes

--- apps/backend-func/src/utils/tracing.ts
+++ apps/backend-func/src/utils/tracing.ts
@@ -2,7 +2,7 @@ import { emitCustomEvent } from "@pagopa/azure-tracing/logger";
 
 export const traceEvent =
   <T>(input: T) =>
-  (caller: string, eventName: string, data: unknown) => {
-    emitCustomEvent(eventName, { data: JSON.stringify(data) })(caller);
-    return input;
-  };
+    (caller: string, eventName: string, data: unknown) => {
+      emitCustomEvent(eventName, { caller: caller, data: JSON.stringify(data) })(caller);
+      return input;
+    };
