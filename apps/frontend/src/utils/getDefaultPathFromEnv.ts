efa4ecc2b7f44feee4de5e4046f8323340b62afd
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2025-12-11 10:51:58 +0100

    chore: remove unused feature flags and utils

--- apps/frontend/src/utils/getDefaultPathFromEnv.ts
+++
@@ -1,7 +0,0 @@
-import { APP_ROUTES } from '../routes/appRoutes';
-import { featureFlags } from './featureFlags';
-
-export const getPathFromEvironment = () => {
-  if (featureFlags.dashboard) return APP_ROUTES.HOME;
-  return APP_ROUTES.SELECT_YEAR;
-};
