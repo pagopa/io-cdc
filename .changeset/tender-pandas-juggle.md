2d754984fdcc50f79e0ab6f69fa0c44c724a716f
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2026-01-26 10:12:10 +0100

    fix: list voucher empty state

---
+++ .changeset/tender-pandas-juggle.md
@@ -0,0 +1,5 @@
+---
+"frontend": patch
+---
+
+fix list voucher empty state
--- apps/frontend/src/components/VoucherList/EmptyVouchers.tsx
+++ apps/frontend/src/components/VoucherList/EmptyVouchers.tsx
@@ -5,13 +5,12 @@ export const EmptyVouchers = ({ type = 'TO_SPEND' }: { type?: 'SPENT' | 'TO_SPEN
   const toSpend = type === 'TO_SPEND';
 
   const title = toSpend
-    ? 'Non sono stati trovati buoni'
+    ? 'Qui troverai i buoni che genererai per i tuoi acquisti.'
     : 'Nessun buono speso. Quando verrà speso un buono, lo vedrai qui.';
-  const icon = toSpend ? 'info' : undefined;
 
   return (
     <Stack minHeight={100} justifyContent="center">
-      <EmptyState icon={icon} title={title} />
+      <EmptyState title={title} />
     </Stack>
   );
 };
