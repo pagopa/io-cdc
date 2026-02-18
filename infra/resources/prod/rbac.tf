e558102d3b004b2fb2d5cf0872e1a610bde3b467
Marco Comi <marco.comi@pagopa.it>
2026-02-18 10:09:03 +0100

    Add roles on staging slots

--- infra/resources/prod/rbac.tf
+++ infra/resources/prod/rbac.tf
@@ -38,6 +38,25 @@ module "roles" {
   ]
 }
 
+module "roles_staging" {
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 1.0"
+
+  principal_id    = module.backend_func.cdc_backend_func.staging_principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+
+  key_vault = [
+    {
+      name                = module.key_vaults.key_vault_cdc.name
+      resource_group_name = module.key_vaults.key_vault_cdc.resource_group_name
+      description         = "Allow CDC Backend Function (staging slot) to read secrets from Key Vault"
+      roles = {
+        secrets = "reader"
+      }
+    }
+  ]
+}
+
 module "support_func_roles" {
   source  = "pagopa-dx/azure-role-assignments/azurerm"
   version = "~> 1.0"
@@ -50,6 +69,25 @@ module "support_func_roles" {
       name                = module.key_vaults.key_vault_cdc.name
       resource_group_name = module.key_vaults.key_vault_cdc.resource_group_name
       description         = "Allow Support Function to read secrets from Key Vault"
+      roles = {
+        secrets = "reader"
+      }
+    }
+  ]
+}
+
+module "support_func_staging_roles" {
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 1.0"
+
+  principal_id    = module.support_func.cdc_support_func.staging_principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+
+  key_vault = [
+    {
+      name                = module.key_vaults.key_vault_cdc.name
+      resource_group_name = module.key_vaults.key_vault_cdc.resource_group_name
+      description         = "Allow Support Function (staging slot) to read secrets from Key Vault"
       roles = {
         secrets = "reader"
       }
