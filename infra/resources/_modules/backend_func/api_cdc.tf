b7873abbef21f7454b36603c09f554654027b78c
Marco Comi <marco.comi@pagopa.it>
2026-02-20 10:35:08 +0100

    Get URL from Key Vault reference

--- infra/resources/_modules/backend_func/api_cdc.tf
+++ infra/resources/_modules/backend_func/api_cdc.tf
@@ -70,8 +70,10 @@ resource "azurerm_api_management_named_value" "cdc_backend_func_url" {
   api_management_name = azurerm_api_management_api.cdc_v1.api_management_name
   resource_group_name = azurerm_api_management_api.cdc_v1.resource_group_name
   display_name        = "io-cdc-backend-func-url"
-  value               = var.cdc_backend_func_url
-  secret              = "true"
+  secret              = true
+  value_from_key_vault {
+    secret_id = var.cdc_backend_func_url
+  }
 }
 
 resource "azurerm_api_management_named_value" "cdc_backend_func_key" {
