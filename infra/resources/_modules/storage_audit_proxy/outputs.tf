23ca5ec2d6e8bc8418499e7c496a32b1ff445681
michaeldisaro <michael.disaro@gmail.com>
2026-03-02 16:07:36 +0100

    (fix): Fixed typos

--- infra/resources/_modules/storage_audit_proxy/outputs.tf
+++ infra/resources/_modules/storage_audit_proxy/outputs.tf
@@ -1,8 +1,8 @@
 output "immutable_cdc_audit_logs_storage_proxy" {
   value = {
-    id                  = module.immutable_cdc_audit_logs_proxy_storage.id
-    name                = module.immutable_cdc_audit_logs_proxy_storage.name
-    resource_group_name = module.immutable_cdc_audit_logs_proxy_storage.resource_group_name
-    primary_web_host    = module.immutable_cdc_audit_logs_proxy_storage.primary_web_host
+    id                  = module.immutable_cdc_audit_logs_storage_proxy.id
+    name                = module.immutable_cdc_audit_logs_storage_proxy.name
+    resource_group_name = module.immutable_cdc_audit_logs_storage_proxy.resource_group_name
+    primary_web_host    = module.immutable_cdc_audit_logs_storage_proxy.primary_web_host
   }
 }
