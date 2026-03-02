050e139af2a0ac094688b84607755a13ce813923
michaeldisaro <michael.disaro@gmail.com>
2026-03-02 15:22:38 +0100

    (fix): Fixed outputs

--- infra/resources/_modules/storage_audit_proxy/outputs.tf
+++ infra/resources/_modules/storage_audit_proxy/outputs.tf
@@ -1,8 +1,8 @@
 output "immutable_cdc_audit_logs_storage_proxy" {
   value = {
-    id                  = module.immutable_cdc_audit_logs_storage.id
-    name                = module.immutable_cdc_audit_logs_storage.name
-    resource_group_name = module.immutable_cdc_audit_logs_storage.resource_group_name
-    primary_web_host    = module.immutable_cdc_audit_logs_storage.primary_web_host
+    id                  = module.immutable_cdc_audit_logs__proxy_storage.id
+    name                = module.immutable_cdc_audit_logs_proxy_storage.name
+    resource_group_name = module.immutable_cdc_audit_logs_proxy_storage.resource_group_name
+    primary_web_host    = module.immutable_cdc_audit_logs_proxy_storage.primary_web_host
   }
 }
