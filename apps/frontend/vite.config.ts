713ba937ba7fd32531e303b002c9102ac6ed1b55
Pierpaolo Piras <108515572+PierpaoloPirasSp@users.noreply.github.com>
2026-03-17 16:16:09 +0100

    chore: change build for healthcheck setup (#260)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ---------
    
    Co-authored-by: michaeldisaro <michael.disaro@gmail.com>
    Co-authored-by: Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import typescript from 'vite-plugin-typescript';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react({ jsxImportSource: '@emotion/react' }),
-    typescript(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/healthcheck.txt',
-          dest: 'assets',
-          rename: 'healthcheck.txt',
-        },
-      ],
-    }),
-  ],
+  plugins: [react({ jsxImportSource: '@emotion/react' }), typescript()],
   resolve: {
     dedupe: [
       'react',
